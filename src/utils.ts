7f6b2b880d16ac7303d41082e6d92ceffeb41316
Huynh Duc Dung <dunghd.it@gmail.com>
2026-01-31 22:13:44 +0800

    feat: add utility function for binary detection

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,13 @@
+import { execFile } from 'node:child_process'
 import { defineLogger } from 'reactive-vscode'
 import { displayName } from './generated/meta'
 
 export const logger = defineLogger(displayName)
+
+export function checkBinaryExists(binary: string): Promise<boolean> {
+  return new Promise((resolve) => {
+    execFile('which', [binary], (error) => {
+      resolve(!error)
+    })
+  })
+}
